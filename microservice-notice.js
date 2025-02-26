42201b6c1f5b07bf8ec3c00bec7af1e477c02cd0
Manu K N <201910101325@presidencyuniversity.in>
2025-02-26 16:55:36 +0530

    added post service

---
+++ microservice-notice.js
@@ -0,0 +1,7 @@
+//In microservice architevture the main advantages are
+// -> Scalabelity and also each microserices are lossly coppulted⭐
+// -> Technology flexibilty ex : for serarch service we can use elastic searching⭐ and also in media service we can use diffrent tech related to media 
+// -> Easier maintenance and upload updates -> our service can updated or maintained independently
+
+// also the project planing graph is in the 5 hours 15 mins timeline(for reference)
+
